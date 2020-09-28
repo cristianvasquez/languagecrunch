c2b1a906150bc830f2fc87fccee7b7ad48e75a9a
Cristian Vasquez <cristianvasquez@gmail.com>
2020-09-28 19:13:43 +0200

    enable cors

--- src/main.py
+++ src/main.py
@@ -130,6 +130,17 @@ def parse_sentence():
         logging.info(sent, " -> ", sent.root)
 
 
+@app.hook('after_request')
+def enable_cors():
+    """
+    You need to add some headers to each request.
+    Don't use the wildcard '*' for Access-Control-Allow-Origin in production.
+    """
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+
 @route('/nlp/coref', method="GET")
 def coreferences():
     sentences = request.query['sentence']
